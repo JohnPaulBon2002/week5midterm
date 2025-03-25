55ed1a8d103f46a8df037734362031c8784ae3fa
JohnPaulBon2002 <jhopjhop209@gmail.com>
2025-03-25 11:09:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index5.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, () => {
